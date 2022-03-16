a0ec71048f51c7c9bbd7892f71768587a145bc5b
Claudia G. Davila Rios <cdavilarios@users.noreply.github.com>
2022-03-15 21:38:54 -0500

    min 32 - till digits and index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [digits, setDigits] = useState([4, 1, 5]);
+
+  let setDigitAtIndex = (digit, idx) => {
+    setDigits([...digits.slice(0, idx), digit, ...digits.slice(idx + 1)]);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <section>
+      <h1>Hello World</h1>
+      <div style={{ display: "flex" }}>
+        <input type="number" value={digits[0]} onChange={(event) => {}}></input>
+        <input type="number" value={digits[1]}></input>
+        <input type="number" value={digits[2]}></input>
+        <button> Press me! </button>
+      </div>
+    </section>
   );
 }
